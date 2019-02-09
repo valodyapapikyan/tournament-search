e5e2609ddafeef4ed41672371bdc94f0e899c466
valodya <valodyapapikyan@gmail.com>
2019-02-09 22:30:09 +0400

    code formatting

--- src/components/common/list-item/index.js
+++ src/components/common/list-item/index.js
@@ -1,32 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import List from '@material-ui/core/List';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import ListItem from '@material-ui/core/ListItem';
+import List from "@material-ui/core/List";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import ListItem from "@material-ui/core/ListItem";
 
-const ResultItem = ({item, clickHandler, ...props}) => {
+const ResultItem = ({ item, clickHandler, ...props }) => {
+  const { image, title, description } = item;
 
-    const {image, title, description} = item;
+  const onClick = () => clickHandler(item);
 
-    const onClick = () => clickHandler(item);
-
-
-    return (
-        <List>
-            <ListItem
-                onClick={!props.withRemoveIcon ? onClick : null}
-                className={props.className}
-            >
-                <Avatar> <img src={image} alt=""/> </Avatar>
-                <ListItemText
-                    primary={title}
-                    secondary={description}
-                />
-                {props.withRemoveIcon ? <span className='remove-btn' onClick={onClick}>X</span> : null}
-            </ListItem>
-        </List>
-    )
+  return (
+    <List>
+      <ListItem
+        onClick={!props.withRemoveIcon ? onClick : null}
+        className={props.className}
+      >
+        <Avatar>
+          {" "}
+          <img src={image} alt="" />{" "}
+        </Avatar>
+        <ListItemText primary={title} secondary={description} />
+        {props.withRemoveIcon ? (
+          <span className="remove-btn" onClick={onClick}>
+            X
+          </span>
+        ) : null}
+      </ListItem>
+    </List>
+  );
 };
 
 export default ResultItem;
--- src/components/search-input/index.js
+++ src/components/search-input/index.js
@@ -1,31 +1,27 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
 
-const SearchInput = ({onSearch}) => {
+const SearchInput = ({ onSearch }) => {
+  let timeout = null;
 
-    let timeout = null;
+  const handleChange = event => {
+    clearTimeout(timeout);
+    let value = event.target.value;
+    timeout = setTimeout(() => value.length > 2 && onSearch(value), 100);
+  };
 
-    const handleChange = (event) => {
-        clearTimeout(timeout);
-        let value = event.target.value;
-        timeout = setTimeout(() => value.length > 2 && onSearch(value), 100);
-    }
-
-    return (
-      
-            <div className='search-input'>
-                <TextField
-                    fullWidth
-                    onKeyUp={handleChange}
-                    id="standard-search"
-                    label="Search field"
-                    type="search"
-                    margin="normal"
-                />
-            </div>
-      
-    );
+  return (
+    <div className="search-input">
+      <TextField
+        fullWidth
+        onKeyUp={handleChange}
+        id="standard-search"
+        label="Search field"
+        type="search"
+        margin="normal"
+      />
+    </div>
+  );
 };
 
-export default SearchInput
-
+export default SearchInput;
